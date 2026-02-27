ea76799386a9d2cc2dbf5d252af03427c0518927
Sushil Kumar <112944863+Sushilgola@users.noreply.github.com>
2026-02-27 20:43:52 +0530

    Add files via upload

---
+++ oddEven.java
@@ -0,0 +1,26 @@
+/*  import java.util.*;
+public class oddEven
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int even = 0;
+        int odd = 0;
+        int n = 5;
+        int[] arr = new int[n];
+        
+        for (int i=0; i<n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+    }
+}
+
+public class test
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}  */
